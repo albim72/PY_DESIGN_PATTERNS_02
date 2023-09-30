2d5d3ee28f59be398881ba7b806a8ab017dde64a
albim72 <marcin.albiniak72@gmail.com>
2023-09-30 14:41:14 +0200

    Update bounded.py

--- RESISTOR_PROPERTY/bounded.py
+++ RESISTOR_PROPERTY/bounded.py
@@ -1,6 +1,9 @@
 from resistor import Resistor
 
 class BoundedResistance(Resistor):
+    stohms = 4E2
+    nazwa = "czerwony"
+
     def __init__(self, ohms):
         super().__init__(ohms)
 
@@ -13,3 +16,15 @@ class BoundedResistance(Resistor):
         if ohms <= 0:
             raise ValueError("Wartość oporności musi być większa od 0!")
         self._ohms = ohms
+
+    @property
+    def doubleohms(self):
+        return 2*self.stohms
+
+    @property
+    def koloropornika(self):
+        return self.nazwa
+
+    @koloropornika.setter
+    def koloropornika(self,nowykolor):
+        self.nazwa = nowykolor
