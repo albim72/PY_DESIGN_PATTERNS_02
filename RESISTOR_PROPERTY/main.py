37d367d4afd04bfbbc621b79b893cc04f2cf0153
albim72 <marcin.albiniak72@gmail.com>
2023-09-30 14:03:50 +0200

    Update main.py

--- RESISTOR_PROPERTY/main.py
+++ RESISTOR_PROPERTY/main.py
@@ -1,4 +1,5 @@
 from oldresistor import OldResistor
+from resistor import Resistor
 
 print("______________ klasa OldResistor _________________")
 r0= OldResistor(10.2E2)
@@ -7,3 +8,11 @@ print(r0._ohms)
 print(r0.get_ohms())
 r0.set_ohms(2.88E3)
 print(r0.get_ohms())
+
+print("______________ klasa Resistor _________________")
+r1 = Resistor(50.1E3)
+print(r1.ohms)
+r1.ohms = 1E2
+print(r1.ohms)
+print(f'układ elektryczny:\noporność: {r1.ohms} omów,\nnapięcie prądu: {r1.voltage} V,\n'
+      f'natężenie prądu: {r1.current} A')
