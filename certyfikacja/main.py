2e00d9840a317dcc7f8b8766b8dd9a9e3f58597b
albim72 <marcin.albiniak72@gmail.com>
2023-09-30 15:09:05 +0200

    Update main.py

--- certyfikacja/main.py
+++ certyfikacja/main.py
@@ -1,5 +1,6 @@
 from ocenaprojektu import Projekt
 from exam_grade import Exam
+from weakrefdict import ExamR
 
 print('_________ klasa Projekt ______________')
 g = Projekt()
@@ -26,3 +27,22 @@ print(f'egzamin -> drugie podejście -> 1: {e2.first_part}, 2: {e2.second_part},
 print(f'dane z archiwum -> 1 podejście...')
 
 print(f'egzamin -> pierwsze podejście -> 1: {e1.first_part}, 2: {e1.second_part}, 3:{e1.third_part}')
+
+print('_________ klasa ExamR ______________')
+e1 = ExamR()
+e1.first_part = 23
+e1.second_part = 11
+e1.third_part = 41
+
+print(f'egzamin R -> pierwsze podejście -> 1: {e1.first_part}, 2: {e1.second_part}, 3:{e1.third_part}')
+
+e2 = ExamR()
+e2.first_part = 67
+e2.second_part = 88
+e2.third_part = 58
+
+print(f'egzamin R -> drugie podejście -> 1: {e2.first_part}, 2: {e2.second_part}, 3:{e2.third_part}')
+
+print(f'dane z archiwum R -> 1 podejście...')
+
+print(f'egzamin R -> pierwsze podejście -> 1: {e1.first_part}, 2: {e1.second_part}, 3:{e1.third_part}')
