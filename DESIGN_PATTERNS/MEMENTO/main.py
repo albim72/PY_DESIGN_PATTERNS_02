2b9b48b68cc544d88672b4d53959cbf02ddefe55
albim72 <marcin.albiniak72@gmail.com>
2023-10-01 13:52:22 +0200

    Update main.py

--- DESIGN_PATTERNS/MEMENTO/main.py
+++ DESIGN_PATTERNS/MEMENTO/main.py
@@ -7,5 +7,21 @@ class FileWriterUtility:
     def __init__(self,file):
         self.file = file
         self.content = ""
+
+    def write(self,string):
+        self.content += string
         
+    def save(self):
+        return Memento(self.file,self.content)
     
+    def undo(self,memento):
+        self.content = memento.content
+        
+
+class FileWriterCaretaker:
+    def save(self,writer):
+        self.obj = writer.save()
+        
+    def undo(self,writer):
+        writer.undo(self.obj)
+        
