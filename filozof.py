e08d3b8f25c0ee785fa3bc847a26acdc0d84ec68
albim72 <marcin.albiniak72@gmail.com>
2023-10-01 09:17:06 +0200

    Create filozof.py

---
+++ filozof.py
@@ -0,0 +1,37 @@
+odp = input("Czy Ziemia jest płaska? Chcesz znać odpowiedź? (t/n) ")
+
+if odp.lower() == "t":
+    required = True
+else:
+    required = False
+
+def odpowiedz(self,*args):
+    return "Tak! Ziemia jest płaska!"
+
+def brak(self):
+    return "brak odpowiedzi..."
+
+class SednoOdpowiedzi(type):
+    def __init__(cls,clsname,bases,attrs):
+        if required:
+            cls.odpowiedz = odpowiedz
+        else:
+            cls.odpowiedz = brak
+
+class Arystoteles(metaclass=SednoOdpowiedzi):
+    pass
+
+class Platon(metaclass=SednoOdpowiedzi):
+    pass
+
+class SwTomasz(metaclass=SednoOdpowiedzi):
+    pass
+
+fil1 = Arystoteles()
+print(f'Filozof {fil1.__class__.__name__} twierdzi: {fil1.odpowiedz()}')
+
+fil2 = Platon()
+print(f'Filozof {fil2.__class__.__name__} twierdzi: {fil2.odpowiedz()}')
+
+fil3 = SwTomasz()
+print(f'Filozof {fil3.__class__.__name__} twierdzi: {fil3.odpowiedz()}')
