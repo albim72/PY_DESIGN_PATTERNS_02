79621580ec0274fa0e62aa43d01af823e5f58f9d
albim72 <marcin.albiniak72@gmail.com>
2023-10-01 16:17:39 +0200

    Update main.py

--- DESIGN_PATTERNS/obserwator/main.py
+++ DESIGN_PATTERNS/obserwator/main.py
@@ -2,3 +2,45 @@ from __future__ import annotations
 from abc import ABC, abstractmethod
 from random import randrange
 from typing import List
+
+class Subject(ABC):
+    @abstractmethod
+    def attach(self,observer:Observer) -> None:
+        pass
+    
+    @abstractmethod
+    def detach(self,observer:Observer) -> None:
+        pass
+    
+    @abstractmethod
+    def notify(self) -> None:
+        pass
+        
+     
+     
+class ConcreteSubject(Subject):
+    _state:int = None
+    _observers: List[Observer] = []
+
+    def attach(self, observer: Observer) -> None:
+        print("Subject: Attached an observer!")
+        self._observers.append(observer)
+
+    def detach(self, observer: Observer) -> None:
+        self._observers.remove(observer)
+
+    def notify(self) -> None:
+        for observer in self._observers:
+            observer.update(self)
+            
+    def some_business_logic(self)->None:
+        print("\nSubject: I'm doing something important!")
+        self._state = randrange(0,10)
+        print(f"Subject: My state just changed to: {self._state}")
+        self.notify()
+
+
+class Observer(ABC):
+    @abstractmethod
+    def update(self,subject:Subject) -> None:
+        pass
