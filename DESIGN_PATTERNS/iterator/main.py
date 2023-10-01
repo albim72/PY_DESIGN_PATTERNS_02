dc9af76b5591f02ab61b88dbcc06a4bd5d7cb90e
albim72 <marcin.albiniak72@gmail.com>
2023-10-01 15:56:51 +0200

    Update main.py

--- DESIGN_PATTERNS/iterator/main.py
+++ DESIGN_PATTERNS/iterator/main.py
@@ -2,16 +2,23 @@ class FootballTeamIterator:
     def __init__(self,members):
         self.members = members
         self.index = 0
-        
+
     def __iter__(self):
         return self
-    
+
     def __next__(self):
         if self.index < len(self.members):
             val = self.members[self.index]
             self.index += 1
             return val
-        
+
         else:
             raise StopIteration()
+
+class FootballTeam:
+    def __init__(self,members):
+        self.members = members
         
+    def __iter__(self):
+        return FootballTeamIterator(self.members)
+    
