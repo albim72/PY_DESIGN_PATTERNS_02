3fa7a3d05463500b0bcecde5b88ae2f35df0943b
albim72 <marcin.albiniak72@gmail.com>
2023-10-01 15:00:31 +0200

    Update main.py

--- DESIGN_PATTERNS/BRIDGE/main.py
+++ DESIGN_PATTERNS/BRIDGE/main.py
@@ -5,11 +5,19 @@ import urllib.request
 class ResourceContent:
     def __init__(self,imp):
         self._imp = imp
-        
+
     def show_content(self,path):
         self._imp.fetch(path)
-        
+
 class ResourceContentFetcher(metaclass=ABCMeta):
     @abstractmethod
     def fetch(self,path):
         pass
+    
+class URLFetcher(ResourceContentFetcher):
+    def fetch(self, path):
+        req = urllib.request.Request(path)
+        with urllib.request.urlopen(req) as response:
+            if response.code == 200:
+                this_page = response.read()
+                print(this_page)
