c8cc2ce986f38a54689081ec22638935bf238113
albim72 <marcin.albiniak72@gmail.com>
2023-10-01 16:01:36 +0200

    Update main.py

--- DESIGN_PATTERNS/iterator/main.py
+++ DESIGN_PATTERNS/iterator/main.py
@@ -18,7 +18,20 @@ class FootballTeamIterator:
 class FootballTeam:
     def __init__(self,members):
         self.members = members
-        
+
     def __iter__(self):
         return FootballTeamIterator(self.members)
-    
+
+def main():
+    members = [f'gracz {str(x)}' for x in range(1,23)]
+    members = members + ["trener1","trener2","trener3","fizjoterapeuta","rzecznik"]
+    team = FootballTeam(members)
+    team_it = iter(team)
+
+    for member in team:
+        print(member)
+
+
+if __name__ == '__main__':
+    main()
+
