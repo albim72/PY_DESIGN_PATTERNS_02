f7864a988bb47b073bce2c2416bc4f40d28a4ba2
albim72 <marcin.albiniak72@gmail.com>
2023-10-01 15:48:02 +0200

    Update main.py

--- DESIGN_PATTERNS/pylek/main.py
+++ DESIGN_PATTERNS/pylek/main.py
@@ -12,11 +12,50 @@ class Car:
             cls.pool[car_type] = obj
             obj.car_type = car_type
         return obj
-    
+
     def render(self,color,x,y):
         type = self.car_type
         msg = f'reneder a car type {type} and {color} at ({x},{y})'
         print(msg)
-        
+
 def main():
-    
+    rnd = random.Random()
+    colors = 'white black silver gray red blue brown beige yellow green'.split()
+    min_point,max_point = 0,100
+    car_counter = 0
+    for _ in range(10):
+        c1 = Car(CarType.subcompact)
+        c1.render(random.choice(colors),
+                  rnd.randint(min_point,max_point),
+                  rnd.randint(min_point,max_point)
+                  )
+        car_counter +=1
+
+    for _ in range(3):
+        c2 = Car(CarType.compact)
+        c2.render(random.choice(colors),
+                  rnd.randint(min_point,max_point),
+                  rnd.randint(min_point,max_point)
+                  )
+        car_counter +=1
+
+    for _ in range(5):
+        c3 = Car(CarType.SUV)
+        c3.render(random.choice(colors),
+                  rnd.randint(min_point,max_point),
+                  rnd.randint(min_point,max_point)
+                  )
+        car_counter +=1
+
+    print(f'cars rendered: {car_counter}')
+    print(f'cars actually created: {len(Car.pool)}')
+
+    c4 = Car(CarType.subcompact)
+    c5 = Car(CarType.subcompact)
+    c6 = Car(CarType.SUV)
+
+    print(f'{id(c4)} == {id(c5)} ? {id(c5)} == {id(c4)} ')
+    print(f'{id(c6)} == {id(c5)} ? {id(c5)} == {id(c6)} ')
+
+if __name__ == '__main__':
+    main()
