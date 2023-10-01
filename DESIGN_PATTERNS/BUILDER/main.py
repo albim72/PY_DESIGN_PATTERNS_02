a0fd0c478a87f3279597218c5217b4d37395f319
albim72 <marcin.albiniak72@gmail.com>
2023-10-01 10:30:48 +0200

    Create main.py

---
+++ DESIGN_PATTERNS/BUILDER/main.py
@@ -0,0 +1,20 @@
+from director import Director
+from concretebuilder1 import ConceteBuilder1
+
+director = Director()
+builder = ConceteBuilder1()
+
+director.builder = builder
+
+print("\n produkt podstawowy:")
+director.build_minimal_version()
+builder.product.list_parts()
+
+print("\n produkt pełny:")
+director.build_full_version()
+builder.product.list_parts()
+
+print("\n produkt użytkownika:")
+builder.produce_part_a()
+builder.produce_part_c()
+builder.product.list_parts()
